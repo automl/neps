dabcb2affe0ac3835943fd6b4cf1ec43034aec23
schrodi <schrodi@cs.uni-freiburg.de>
2021-09-25 13:57:57 +0200

    Changed computational flow to be compatible with new evolutionary acqusition optimizers etc.

--- comprehensive_nas/bayesian_optimization/acquisition_function_optimization/base_acq_optimizer.py
+++ comprehensive_nas/bayesian_optimization/acquisition_function_optimization/base_acq_optimizer.py
@@ -8,16 +8,22 @@ except ModuleNotFoundError:
 
     raise ModuleNotFoundError(error_message)
 
+from ..acqusition_functions.base_acqusition import BaseAcquisition
+
 
 class AcquisitionOptimizer:
-    def __init__(self, objective):
+    def __init__(self, objective, acquisition_function: BaseAcquisition):
         self.objective = objective
+        self.acquisition_function = acquisition_function
         self.x: Iterable = []
         self.y: Iterable = []
 
     @abstractmethod
     def sample(self, pool_size: int) -> list:
         raise NotImplementedError
+
+    def reset_surrogate_model(self, surrogate_model) -> None:
+        self.acquisition_function.reset_surrogate_model(surrogate_model)
 
     def reset_XY(self, x: Iterable, y: Union[Iterable, torch.Tensor]) -> None:
         self.x = x
--- comprehensive_nas/bayesian_optimization/acquisition_function_optimization/random_sampler.py
+++ comprehensive_nas/bayesian_optimization/acquisition_function_optimization/random_sampler.py
@@ -1,14 +1,20 @@
 from copy import deepcopy
+from typing import Tuple
 
+import numpy as np
+
+from ..acqusition_functions.base_acqusition import BaseAcquisition
 from .base_acq_optimizer import AcquisitionOptimizer
 
 
 class RandomSampler(AcquisitionOptimizer):
-    def __init__(self, objective, patience: int = 100):
-        super().__init__(objective=objective)
+    def __init__(
+        self, objective, acquisition_function: BaseAcquisition = None, patience: int = 100
+    ):
+        super().__init__(objective, acquisition_function)
         self.patience = patience
 
-    def sample(self, pool_size: int) -> list:
+    def create_pool(self, pool_size: int) -> list:
         pool = []
         while len(pool) < pool_size:
             rand_config = deepcopy(self.objective)
@@ -25,5 +31,20 @@ class RandomSampler(AcquisitionOptimizer):
                     f"Cannot sample valid random architecture in {self.patience} tries!"
                 )
             pool.append(rand_config)
-
         return pool
+
+    def sample(
+        self, pool_size: int, batch_size: int = None
+    ) -> Tuple[list, list, np.ndarray]:
+        pool = self.create_pool(pool_size)
+
+        if batch_size is None:
+            return pool
+
+        if batch_size is not None and self.acquisition_function is None:
+            raise Exception(f"Random sampler has no acquisition function!")
+
+        samples, acq_vals, _ = self.acquisition_function.propose_location(
+            top_n=batch_size, candidates=pool
+        )
+        return samples, pool, acq_vals
