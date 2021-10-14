e3da0b4d8e3cc7d247cae7766313ab72025f7b8b
schrodi <schrodi@cs.uni-freiburg.de>
2021-10-14 13:06:36 +0200

    Add seed handling to base class

--- comprehensive_nas/evaluation/objective.py
+++ comprehensive_nas/evaluation/objective.py
@@ -4,13 +4,17 @@ import numpy as np
 
 
 class Objective:
-    def __init__(self, log_scale: bool, negative: bool = False) -> None:
+    def __init__(self, seed: int, log_scale: bool, negative: bool = False) -> None:
+        self.seed = seed
         self.log_scale = log_scale
         self.negative = negative
 
     @abstractmethod
     def __call__(self, config, mode, **kwargs):
         raise NotImplementedError
+
+    def set_seed(self, seed: int):
+        self.seed = seed
 
     def transform(self, val):
         if self.log_scale:
