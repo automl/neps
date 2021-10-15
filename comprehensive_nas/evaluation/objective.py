aa411f102174fcb00427dbbe865dadf877b0c3ea
schrodi <schrodi@cs.uni-freiburg.de>
2021-10-15 14:38:03 +0200

    Moved utility class to hierarchical benchmarks

--- comprehensive_nas/evaluation/objective.py
+++ comprehensive_nas/evaluation/objective.py
@@ -1,4 +1,3 @@
-import time
 from abc import abstractmethod
 
 import numpy as np
@@ -30,24 +29,3 @@ class Objective:
         if self.log_scale:
             val = np.exp(val)
         return val
-
-
-class ObjectiveWithAPI(Objective):
-    def __init__(self, api) -> None:
-        super().__init__(None, None, None)
-        self.api = api
-
-    def __call__(self, config):
-        _config = config.get_model_for_evaluation()
-        start = time.time()
-        loss = self.api.eval(_config)
-        end = time.time()
-        return {
-            "loss": self.api.transform(loss),
-            "info_dict": {
-                "config_id": config.id,
-                "val_score": loss,
-                "test_score": self.api.test(_config),
-                "train_time": end - start,
-            },
-        }
