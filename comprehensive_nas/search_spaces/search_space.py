551405e355b91d69e20290a988ce319a29ea5cd3
worstseed <maciejanowski@gmail.com>
2021-11-02 13:51:49 +0100

    add get_hp_by_name for search_space object by holding all hps in a dictionary

--- comprehensive_nas/search_spaces/search_space.py
+++ comprehensive_nas/search_spaces/search_space.py
@@ -1,4 +1,4 @@
-import itertools
+from collections import OrderedDict
 
 import numpy as np
 
@@ -7,26 +7,29 @@ from .graph_dense.graph_dense import GraphHyperparameter
 
 class SearchSpace:
     def __init__(self, *hyperparameters):
-        self.hps = []
-        self.graphs = []
+        self._num_hps = len(hyperparameters)
+        self._hyperparameters = OrderedDict()
+        self._hps = []
+        self._graphs = []
 
         for hyperparameter in hyperparameters:
+            self._hyperparameters[hyperparameter.name] = hyperparameter
             if isinstance(hyperparameter, GraphHyperparameter):
-                self.graphs.append(hyperparameter)
+                self._graphs.append(hyperparameter)
             else:
-                self.hps.append(hyperparameter)
+                self._hps.append(hyperparameter)
 
         self.name = ""
 
     def sample(self):
-        for hyperparameter in set(itertools.chain.from_iterable((self.hps, self.graphs))):
+        for hyperparameter in self._hyperparameters.values():
             hyperparameter.sample()
 
         self.name = self.parse()
 
     def mutate(self, config=None, mutate_probability_per_hyperparameter=1.0, patience=50):
         new_config = []
-        for hyperparameter in set(itertools.chain.from_iterable((self.hps, self.graphs))):
+        for hyperparameter in self._hyperparameters.values():
             if np.random.random() < mutate_probability_per_hyperparameter:
                 while patience > 0:
                     try:
@@ -43,23 +46,29 @@ class SearchSpace:
 
     def parse(self):
         config = ""
-        if self.graphs is not None:
-            if not isinstance(self.graphs, list):
-                # Convert to a singleton list
-                gl = [self.graphs]
-            else:
-                gl = self.graphs
-            config = config.join([g.name for g in gl])
-        if self.hps is not None:
-            config = config.join(["{}-{}".format(hp.name, hp.value) for hp in self.hps])
+        for hp in self._hyperparameters.values():
+            config += (
+                hp.name
+                if isinstance(hp, GraphHyperparameter)
+                else "{}-{}".format(hp.name, hp.value)
+            )
+            config += "_"
         return config
 
     @property
     def id(self):
         return self.name
 
-    def get_graph(self):
-        return [graph.value for graph in self.graphs]
+    def get_graphs(self):
+        return [graph.value for graph in self._graphs]
 
     def get_hps(self):
-        return [hp.value for hp in self.hps]
+        return [hp.value for hp in self._hps]
+
+    def get_hyperparameter_by_name(self, name: str):
+        hp = self._hyperparameters.get(name)
+
+        if hp is None:
+            raise KeyError("Hyperparameter is not a part of the search space!")
+
+        return hp
