ad67e6f1ce098fd4d4f12b9c98db57474e36cd26
worstseed <maciejanowski@gmail.com>
2021-11-12 19:18:29 +0100

    add HPO example (counting ones) using metahyper

--- cnas_examples/hyperparameters/optimize.py
+++ cnas_examples/hyperparameters/optimize.py
@@ -1,34 +1,52 @@
 import comprehensive_nas as cnas
+from comprehensive_nas.optimizers.bayesian_optimization.kernels import (
+    StationaryKernelMapping,
+)
+import numpy as np
+import time
+import logging
 
 
 def run_pipeline(  # pylint: disable=unused-argument
     config, config_working_directory, previous_working_directory
 ):
-    if config["operation"] == "multiply":
-        return config["operant_a"] * config["operant_b"]
-    elif config["operation"] == "add":
-        return config["operant_a"] + config["operant_b"]
-    else:
-        raise ValueError
+    x = np.array(config.get_hps(), dtype=float)
+    start = time.time()
+    y = -float(np.sum(x))
+    end = time.time()
+
+    return {
+        "loss": y,
+        "info_dict": {
+            "config_id": config.id,
+            "val_score": y,
+            "test_score": y,
+            "train_time": end - start,
+        },
+    }
 
 
 if __name__ == "__main__":
-    # We want this API at some point:
-    # pipeline_space = cnas.SearchSpace(
-    #     operation=cnas.Categorical(choices={"multiply", "add"}),
-    #     operant_a=cnas.Integer(lower=1, upper=100),
-    #     operant_b=cnas.Float(lower=1, upper=100, log=True),
-    # )
     pipeline_space = cnas.SearchSpace(
-        cnas.CategoricalHyperparameter(name="operation", choices={"multiply", "add"}),
-        cnas.IntegerHyperparameter(name="operant_a", lower=1, upper=100),
-        cnas.FloatHyperparameter(name="operant_b", lower=1, upper=100, log=True),
+        cnas.HyperparameterMapping['float'](name='x1', lower=0, upper=1, log=False),
+        cnas.HyperparameterMapping['float'](name='x2', lower=0, upper=1, log=False),
+        cnas.HyperparameterMapping['float'](name='x3', lower=0, upper=1, log=False),
+        cnas.HyperparameterMapping['float'](name='x4', lower=0, upper=1, log=False),
+        cnas.HyperparameterMapping['float'](name='x5', lower=0, upper=1, log=False),
+        cnas.HyperparameterMapping['categorical'](name='x6', choices=[0, 1]),
+        cnas.HyperparameterMapping['categorical'](name='x7', choices=[0, 1]),
+        cnas.HyperparameterMapping['categorical'](name='x8', choices=[0, 1]),
+        cnas.HyperparameterMapping['categorical'](name='x9', choices=[0, 1]),
+        cnas.HyperparameterMapping['categorical'](name='x10', choices=[0, 1]),
     )
+
+    logging.basicConfig(level=logging.INFO)
     cnas.run_comprehensive_nas(
         run_pipeline=run_pipeline,
         pipeline_space=pipeline_space,
         working_directory="results/dummy_example",
-        n_iterations=10,
+        n_iterations=100,
         searcher="bayesian_optimization",
         overwrite_logging=True,
+        hp_kernels=['m52', 'hm']
     )
