82cb0a283f64ba164605dd2cef5bbf58b9309ddb
worstseed <maciejanowski@gmail.com>
2021-11-24 14:36:41 +0100

    add incumbent printing after a successful run

--- cnas_examples/hyperparameters/optimize.py
+++ cnas_examples/hyperparameters/optimize.py
@@ -1,10 +1,9 @@
-import comprehensive_nas as cnas
-from comprehensive_nas.optimizers.bayesian_optimization.kernels import (
-    StationaryKernelMapping,
-)
-import numpy as np
-import time
 import logging
+import time
+
+import numpy as np
+
+import comprehensive_nas as cnas
 
 
 def run_pipeline(  # pylint: disable=unused-argument
@@ -28,25 +27,31 @@ def run_pipeline(  # pylint: disable=unused-argument
 
 if __name__ == "__main__":
     pipeline_space = cnas.SearchSpace(
-        cnas.HyperparameterMapping['float'](name='x1', lower=0, upper=1, log=False),
-        cnas.HyperparameterMapping['float'](name='x2', lower=0, upper=1, log=False),
-        cnas.HyperparameterMapping['float'](name='x3', lower=0, upper=1, log=False),
-        cnas.HyperparameterMapping['float'](name='x4', lower=0, upper=1, log=False),
-        cnas.HyperparameterMapping['float'](name='x5', lower=0, upper=1, log=False),
-        cnas.HyperparameterMapping['categorical'](name='x6', choices=[0, 1]),
-        cnas.HyperparameterMapping['categorical'](name='x7', choices=[0, 1]),
-        cnas.HyperparameterMapping['categorical'](name='x8', choices=[0, 1]),
-        cnas.HyperparameterMapping['categorical'](name='x9', choices=[0, 1]),
-        cnas.HyperparameterMapping['categorical'](name='x10', choices=[0, 1]),
+        cnas.HyperparameterMapping["float"](name="x1", lower=0, upper=1, log=False),
+        cnas.HyperparameterMapping["float"](name="x2", lower=0, upper=1, log=False),
+        cnas.HyperparameterMapping["float"](name="x3", lower=0, upper=1, log=False),
+        cnas.HyperparameterMapping["float"](name="x4", lower=0, upper=1, log=False),
+        cnas.HyperparameterMapping["float"](name="x5", lower=0, upper=1, log=False),
+        cnas.HyperparameterMapping["categorical"](name="x6", choices=[0, 1]),
+        cnas.HyperparameterMapping["categorical"](name="x7", choices=[0, 1]),
+        cnas.HyperparameterMapping["categorical"](name="x8", choices=[0, 1]),
+        cnas.HyperparameterMapping["categorical"](name="x9", choices=[0, 1]),
+        cnas.HyperparameterMapping["categorical"](name="x10", choices=[0, 1]),
     )
 
     logging.basicConfig(level=logging.INFO)
-    cnas.run_comprehensive_nas(
+    result = cnas.run_comprehensive_nas(
         run_pipeline=run_pipeline,
         pipeline_space=pipeline_space,
         working_directory="results/dummy_example",
         n_iterations=100,
         searcher="bayesian_optimization",
         overwrite_logging=True,
-        hp_kernels=['m52', 'hm']
+        hp_kernels=["m52", "hm"],
     )
+
+    id2config = result.get_id2config_mapping()
+    incumbent = result.get_incumbent_id()
+
+    print("Best found configuration:", id2config[incumbent]["config"])
+    print("A total of %i unique configurations where sampled." % len(id2config.keys()))
