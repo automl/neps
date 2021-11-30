fb51570c30b80b9b4270f92da066d9ef0bcf3dc5
worstseed <maciejanowski@gmail.com>
2021-11-30 17:07:09 +0100

    naming change hyperparameter to parameter (of a search space)

---
+++ comprehensive_nas/search_spaces/parameter.py
@@ -0,0 +1,22 @@
+from abc import abstractmethod
+
+
+class Parameter:
+    def __init__(self):
+        self.value = None
+
+    @abstractmethod
+    def sample(self):
+        raise NotImplementedError
+
+    @abstractmethod
+    def mutate(self, parent=None):
+        raise NotImplementedError
+
+    @abstractmethod
+    def crossover(self, parent1, parent2=None):
+        raise NotImplementedError
+
+    @abstractmethod
+    def create_from_id(self, identifier):
+        raise NotImplementedError
