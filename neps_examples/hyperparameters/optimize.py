f7b4fcf5c8640edd071f8b91a9abb14591595582
Danny Stoll <stolld@cs.uni-freiburg.de>
2021-12-29 02:36:09 +0100

    remove unnecessary args in examples

--- neps_examples/hyperparameters/optimize.py
+++ neps_examples/hyperparameters/optimize.py
@@ -45,8 +45,6 @@ if __name__ == "__main__":
         pipeline_space=pipeline_space,
         working_directory="results/hyperparameters_example",
         n_iterations=20,
-        searcher="random_search",
-        overwrite_logging=True,
         hp_kernels=["m52", "hm"],
         use_new_metahyper=True,
     )
