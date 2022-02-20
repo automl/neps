2cbd8d081e894e0c691c5d4c3f06d6c73f9c7f80
Danny Stoll <stolld@cs.uni-freiburg.de>
2022-02-20 18:20:38 +0100

    make status importable

--- neps/status.py
+++ neps/status.py
@@ -1,63 +1,89 @@
+from __future__ import annotations
+
 import argparse
 import logging
 from pathlib import Path
 
 from . import read_results
+from .utils.result_utils import get_loss
 
-# fmt: off
-parser = argparse.ArgumentParser(
-    prog="python -m neps.status",
-    description="Displays status information about a working directory of a neps.run"
-)
-parser.add_argument("working_directory", type=Path, help="The working directory given to neps.run")
-parser.add_argument("--best_losses", action="store_true", help="Show the trajectory of the best loss across evaluations")
-parser.add_argument("--best_configs", action="store_true", help="Show the trajectory of the best configs and their losses across evaluations")
-parser.add_argument("--all_configs", action="store_true", help="Show all configs and their losses")
-args = parser.parse_args()
-# fmt: on
 
-logging.basicConfig(level=logging.WARN)
+def status(
+    working_directory: str | Path,
+    best_losses: bool = False,
+    best_configs: bool = False,
+    all_configs: bool = False,
+):
+    previous_results, pending_configs, pending_configs_free = read_results(
+        working_directory, logging.getLogger("neps.status")
+    )
+    print(f"#Evaluated configs: {len(previous_results)}")
+    print(f"#Pending configs: {len(pending_configs)}")
+    print(
+        f"#Pending configs with worker: {len(pending_configs) - len(pending_configs_free)}"
+    )
 
-previous_results, pending_configs, pending_configs_free = read_results(
-    args.working_directory, logging.getLogger("neps.status")
-)
-print(f"#Evaluated configs: {len(previous_results)}")
-print(f"#Pending configs: {len(pending_configs)}")
-print(f"#Pending configs with worker: {len(pending_configs) - len(pending_configs_free)}")
+    if len(previous_results) == 0:
+        exit(0)
 
-if len(previous_results) == 0:
-    exit(0)
+    best_loss = float("inf")
+    best_config_id = None
+    best_config = None
+    num_error = 0
+    for config_id, evaluation in previous_results.items():
+        if evaluation.result == "error":
+            num_error += 1
+        if get_loss(evaluation.result) < best_loss:
+            best_loss = get_loss(evaluation.result)
+            best_config = evaluation.config
+            best_config_id = config_id
 
-best_loss = float("inf")
-best_config_id = None
-best_config = None
-for config_id, (config, result) in previous_results.items():
-    if result["loss"] < best_loss:
-        best_loss = result["loss"]
-        best_config = config
-        best_config_id = config_id
+    print(f"#Crashed configs: {num_error}")
 
-print()
-print(f"Best loss: {best_loss}")
-print(f"Best config id: {best_config_id}")
-print(f"Best config: {best_config}")
-
-if args.best_losses:
     print()
-    print("Best loss across evaluations:")
-    best_loss_trajectory = args.working_directory / "best_loss_trajectory.txt"
-    print(best_loss_trajectory.read_text(encoding="utf-8"))
+    print(f"Best loss: {best_loss}")
+    print(f"Best config id: {best_config_id}")
+    print(f"Best config: {best_config}")
 
-if args.best_configs:
-    print()
-    print("Best configs and their losses across evaluations:")
-    print(79 * "-")
-    best_loss_config = args.working_directory / "best_loss_with_config_trajectory.txt"
-    print(best_loss_config.read_text(encoding="utf-8"))
+    if best_losses:
+        print()
+        print("Best loss across evaluations:")
+        best_loss_trajectory = args.working_directory / "best_loss_trajectory.txt"
+        print(best_loss_trajectory.read_text(encoding="utf-8"))
 
-if args.all_configs:
-    print()
-    print("All evaluated configs and their losses:")
-    print(79 * "-")
-    all_loss_config = args.working_directory / "all_losses_and_configs.txt"
-    print(all_loss_config.read_text(encoding="utf-8"))
+    if best_configs:
+        print()
+        print("Best configs and their losses across evaluations:")
+        print(79 * "-")
+        best_loss_config = args.working_directory / "best_loss_with_config_trajectory.txt"
+        print(best_loss_config.read_text(encoding="utf-8"))
+
+    if all_configs:
+        print()
+        print("All evaluated configs and their losses:")
+        print(79 * "-")
+        all_loss_config = args.working_directory / "all_losses_and_configs.txt"
+        print(all_loss_config.read_text(encoding="utf-8"))
+
+    return previous_results, pending_configs
+
+
+if __name__ == "__main__":
+    # fmt: off
+    parser = argparse.ArgumentParser(
+        prog="python -m neps.status",
+        description="Displays status information about a working directory of a neps.run"
+    )
+    parser.add_argument("working_directory", type=Path,
+                        help="The working directory given to neps.run")
+    parser.add_argument("--best_losses", action="store_true",
+                        help="Show the trajectory of the best loss across evaluations")
+    parser.add_argument("--best_configs", action="store_true",
+                        help="Show the trajectory of the best configs and their losses across evaluations")
+    parser.add_argument("--all_configs", action="store_true",
+                        help="Show all configs and their losses")
+    args = parser.parse_args()
+    # fmt: on
+
+    logging.basicConfig(level=logging.WARN)
+    status(args.working_directory, args.best_losses, args.best_configs, args.all_configs)
