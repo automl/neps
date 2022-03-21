2a0ffc669f26f3507a7bb5bd51b1061879a26ac3
Theophane Vallaeys <webalorn@gmail.com>
2022-03-21 16:10:40 +0100

    Adding test for user prior

--- neps_examples/cost_aware/optimize.py
+++ neps_examples/cost_aware/optimize.py
@@ -6,7 +6,9 @@ import numpy as np
 import neps
 
 
-def run_pipeline(working_directory, float1, float2, categorical, integer1, integer2):
+def run_pipeline(
+    working_directory, float1, float2, categorical, integer1, integer2
+):  # pylint: disable=unused-argument
     start = time.time()
     y = -float(np.sum([float1, float2, int(categorical), integer1, integer2]))
     end = time.time()
@@ -18,7 +20,9 @@ def run_pipeline(working_directory, float1, float2, categorical, integer1, integ
 
 pipeline_space = dict(
     float1=neps.FloatParameter(lower=0, upper=1, log=False),
-    float2=neps.FloatParameter(lower=0, upper=10, log=False),
+    float2=neps.FloatParameter(
+        lower=0, upper=10, log=False, default=10, default_confidence="medium"
+    ),
     categorical=neps.CategoricalParameter(choices=[0, 1]),
     integer1=neps.IntegerParameter(lower=0, upper=1, log=False),
     integer2=neps.IntegerParameter(lower=0, upper=1, log=False),
