ce232f4eccf2cb0a02469e466ce17ac434fa5015
Danny <stolld@cs.uni-freiburg.de>
2022-04-21 02:50:42 +0200

    Move multi fidelity to own fil

--- neps/optimizers/__init__.py
+++ neps/optimizers/__init__.py
@@ -5,10 +5,8 @@ from typing import Callable
 
 from .base_optimizer import BaseOptimizer
 from .bayesian_optimization.acquisition_functions.ei_cost_aware import CArBO
-from .bayesian_optimization.optimizer import (
-    BayesianOptimization,
-    BayesianOptimizationMultiFidelity,
-)
+from .bayesian_optimization.multi_fidelity import BayesianOptimizationMultiFidelity
+from .bayesian_optimization.optimizer import BayesianOptimization
 from .random_search.optimizer import RandomSearch
 
 SearcherMapping: dict[str, Callable] = {
