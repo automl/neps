f9f8e1bbcfa43521bdd569c2e6f69401a44945af
neeratyoy <neeratyoy@gmail.com>
2022-04-27 02:11:30 +0200

    Adding get_cost utility

--- neps/utils/result_utils.py
+++ neps/utils/result_utils.py
@@ -10,3 +10,12 @@ def get_loss(result: str | dict | float) -> float | Any:
         return float(result["loss"])
     else:
         return float(result)
+
+
+def get_cost(result: str | dict | float) -> float | Any:
+    if result == "error":
+        return float("inf")
+    elif isinstance(result, dict):
+        return float(result["cost"])
+    else:
+        return float(result)
