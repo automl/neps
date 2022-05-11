d98d323dbe6778b0c392314002ef62bf8dbcc090
worstseed <maciejanowski@gmail.com>
2022-05-12 01:36:07 +0200

    add handling of grid search exhaustion

--- neps/optimizers/grid_search/optimizer.py
+++ neps/optimizers/grid_search/optimizer.py
@@ -27,6 +27,8 @@ class GridSearch(BaseOptimizer):
         self._num_previous_configs = len(previous_results) + len(pending_evaluations)
 
     def get_config_and_ids(self) -> tuple[SearchSpace, str, str | None]:
+        if self._num_previous_configs > len(self.configs_list) - 1:
+            raise ValueError("Grid search exhausted!")
         config = self.configs_list[self._num_previous_configs]
-        config_id = str(self._num_previous_configs + 1)
+        config_id = str(self._num_previous_configs)
         return config.hp_values(), config_id, None
