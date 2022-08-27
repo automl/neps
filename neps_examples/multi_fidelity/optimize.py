4da838ed4addb9a4a73ca554b6e3e36183d5bf39
Neeratyoy Mallik <neeratyoy@gmail.com>
2022-08-27 03:05:05 +0200

    Fixing failing example

--- neps_examples/multi_fidelity/optimize.py
+++ neps_examples/multi_fidelity/optimize.py
@@ -7,13 +7,6 @@ import torch
 import neps
 from neps_examples.multi_fidelity.model_and_optimizer import get_model_and_optimizer
 
-multi_fidelity_algos = [
-    "successive_halving",
-    "successive_halving_prior",
-    "asha",
-    "asha_prior",
-]
-
 
 def run_pipeline(pipeline_directory, previous_pipeline_directory, learning_rate, epoch):
     model, optimizer = get_model_and_optimizer(learning_rate)
@@ -42,36 +35,24 @@ def run_pipeline(pipeline_directory, previous_pipeline_directory, learning_rate,
     return np.log(learning_rate / epoch)  # Replace with actual error
 
 
-def input_args():
-    parser = argparse.ArgumentParser()
-    parser.add_argument(
-        "--algo", type=str, default="successive_halving", choices=multi_fidelity_algos
-    )
-    args = parser.parse_args()
-    return args
+pipeline_space = dict(
+    learning_rate=neps.FloatParameter(
+        lower=1e-4, upper=1e0, log=True, default=1e-1, default_confidence="high"
+    ),
+    epoch=neps.IntegerParameter(lower=1, upper=10, is_fidelity=True),
+)
 
+logging.basicConfig(level=logging.INFO)
 
-if __name__ == "__main__":
-    args = input_args()
-
-    pipeline_space = dict(
-        learning_rate=neps.FloatParameter(
-            lower=1e-4, upper=1e0, log=True, default=1e-1, default_confidence="high"
-        ),
-        epoch=neps.IntegerParameter(lower=1, upper=10, is_fidelity=True),
-    )
-
-    logging.basicConfig(level=logging.INFO)
-
-    searcher = args.algo
-    searcher_kwargs = dict(use_priors=False) if "prior" not in args.algo else dict()
-    searcher_output = f"{args.algo}_example"
-    neps.run(
-        run_pipeline=run_pipeline,
-        pipeline_space=pipeline_space,
-        root_directory=f"results/{searcher_output}",
-        max_evaluations_total=50,
-        searcher=searcher,
-        **searcher_kwargs,
-    )
-    previous_results, pending_configs = neps.status(f"results/{searcher_output}")
+searcher = "successive_halving_prior"
+searcher_kwargs = dict(use_priors=False) if "prior" not in searcher else dict()
+searcher_output = f"{searcher}_example"
+neps.run(
+    run_pipeline=run_pipeline,
+    pipeline_space=pipeline_space,
+    root_directory=f"results/{searcher_output}",
+    max_evaluations_total=50,
+    searcher=searcher,
+    **searcher_kwargs,
+)
+previous_results, pending_configs = neps.status(f"results/{searcher_output}")
