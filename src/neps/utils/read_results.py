ed3126b04f1b846bdd90da1a29454717ab9d6416
Maciej Janowski <maciejanowski@gmail.com>
2022-11-18 13:58:49 +0100

    improve plotting (now through neps.plot)

--- src/neps/utils/read_results.py
+++ src/neps/utils/read_results.py
@@ -3,92 +3,39 @@ from __future__ import annotations
 import os
 
 import numpy as np
-import yaml  # type: ignore
 
-from ..utils.common import AttrDict
-
-SINGLE_FIDELITY_ALGORITHMS = [
-    "random_search",
-    "bayesian_optimization",
-]
-
-
-def load_yaml(filename: str) -> AttrDict:
-    with open(filename, encoding="UTF-8") as f:
-        # https://github.com/yaml/pyyaml/wiki/PyYAML-yaml.load(input)-Deprecation
-        args = yaml.load(f, Loader=yaml.FullLoader)
-    return AttrDict(args)
-
-
-def _get_info_neps(path: str | os.PathLike, seed: str | int | None):
-    if seed is not None:
-        path = os.path.join(path, str(seed), "neps_root_directory")
-
-    with open(
-        os.path.join(path, "all_losses_and_configs.txt"),
-        encoding="UTF-8",
-    ) as f:
-        data = f.readlines()
-    losses = [
-        float(entry.strip().split("Loss: ")[1]) for entry in data if "Loss: " in entry
-    ]
-
-    config_ids = [
-        f"config_{entry.strip().split('Config ID: ')[1]}"
-        for entry in data
-        if "Config ID: " in entry
-    ]
-    info = []
-    result_path = os.path.join(path, "results")
-    for config_id in config_ids:
-        result_yaml = load_yaml(os.path.join(result_path, config_id, "result.yaml"))
-        if "info_dict" in result_yaml:
-            start_time = (
-                result_yaml.info_dict["start_time"]
-                if "start_time" in result_yaml.info_dict
-                else 0.0
-            )
-            end_time = (
-                result_yaml.info_dict["end_time"]
-                if "start_time" in result_yaml.info_dict
-                else 0.0
-            )
-            info.append(
-                dict(
-                    fidelity=result_yaml.info_dict["fidelity"],
-                    cost=result_yaml.info_dict["cost"],
-                    start_time=start_time,
-                    end_time=end_time,
-                    config_id=config_id,
-                )
-            )
-        else:
-            info.append(dict())
-
-    data = list(zip(config_ids, losses, info))  # type: ignore
-
-    return data
+import neps
 
 
 def _get_seed_info(
     path: str | os.PathLike,
     seed: str | int | None,
     key_to_extract: str | None = None,
-    algorithm: str = "random_search",
+    consider_continuations: bool = False,
     n_workers: int = 1,
 ) -> tuple[list[float], list[dict], float]:
-    """Reads and processes data per seed.
+    """Reads and processes data per seed."""
 
-    An `algorithm` needs to be passed to calculate continuation costs.
-    """
+    if seed is not None:
+        path = os.path.join(path, str(seed), "neps_root_directory")
 
-    data = _get_info_neps(path, seed)
+    data = []
+    stats, _ = neps.status(path)
+    for config_id, config_result in stats.items():
+        info = dict()
+        if "info_dict" in config_result.result:
+            info = config_result.result["info_dict"]
+        data.append((config_id, config_result.result["loss"], info))
+
+    data.sort()
+    data = sorted(data, key=lambda x: len(x[0]))
 
     max_cost = None if key_to_extract == "cost" else 0.0
+
     if key_to_extract is not None:
         if n_workers == 1:
             # max_cost only relevant for scaling x-axis when using fidelity on the x-axis
-            if algorithm not in SINGLE_FIDELITY_ALGORITHMS:
+            if consider_continuations:
                 # calculates continuation costs for MF algorithms
                 # NOTE: assumes that all recorded evaluations are black-box evaluations where
                 #   continuations or freeze-thaw was not accounted for during optimization
@@ -135,8 +82,7 @@ def _get_seed_info(
             if max_cost is not None:
                 max_cost += 10.0
 
-    data = [(d[1], d[2]) for d in data]
-    losses, infos = zip(*data)
+    losses, infos = list(zip(*data))[1], list(zip(*data))[2]
     if max_cost is None:
         max_cost = 0.0
 
@@ -146,22 +92,21 @@ def _get_seed_info(
 def process_seed(
     path: str | os.PathLike,
     seed: int | str | None,
-    algorithm: str,
     key_to_extract: str | None = None,
+    consider_continuations: bool = False,
     n_workers: int = 1,
 ) -> tuple[list, list[float | None] | None, float]:
 
-    # `algorithm` is passed to calculate continuation costs
     losses, infos, max_cost = _get_seed_info(
-        path,
-        seed,
-        algorithm=algorithm,
+        path=path,
+        seed=seed,
+        consider_continuations=consider_continuations,
         n_workers=n_workers,
     )
     incumbent = list(np.minimum.accumulate(losses))
     if key_to_extract is not None:
         cost = [i[key_to_extract] for i in infos]
     else:
-        cost = [1.0 for _ in infos]
+        cost = [1.0] * len(infos)
 
     return incumbent, cost, max_cost
