8a6bb6a1aa66d97716d83ac05920aeebc1945153
eddiebergman <eddiebergmanhs@gmail.com>
2023-11-21 09:36:29 +0100

    ci: Fix optional dependancies, use in workflow

--- src/neps/optimizers/bayesian_optimization/models/__init__.py
+++ src/neps/optimizers/bayesian_optimization/models/__init__.py
@@ -10,12 +10,12 @@ from .gp_hierarchy import ComprehensiveGPHierarchy
 try:
     from .deepGP import DeepGP
 except ImportError as e:
-    DeepGP = MissingDependencyError(libname="neps", dep="gpytorch", install_group="experimental", cause=e)
+    DeepGP = MissingDependencyError(libname="neps", dep="gpytorch", install_group="gpytorch", cause=e)
 
 try:
     from .pfn import PFN_SURROGATE  # only if available locally
 except Exception as e:
-    PFN_SURROGATE = MissingDependencyError(libname="neps", dep="pfn", install_group="experimental", cause=e)
+    PFN_SURROGATE = MissingDependencyError(libname="neps", dep="pfn", install_group=None, cause=e)
 
 SurrogateModelMapping = {
     "deep_gp": DeepGP,
