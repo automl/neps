45c293f7940167c163ebcec25ef7714eb2c7fc62
eddiebergman <eddiebergmanhs@gmail.com>
2023-11-21 09:40:41 +0100

    fix: Use __future__ annotations

--- src/metahyper/exceptions.py
+++ src/metahyper/exceptions.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 class MissingDependencyError(Exception):
     """Used whenever an optional dependancy is missing."""
 
--- tests/test_metahyper/test_instance_mapping.py
+++ tests/test_metahyper/test_instance_mapping.py
@@ -1,18 +1,24 @@
-
-from metahyper.utils import instance_from_map
-from metahyper.exceptions import MissingDependencyError
-
 import pytest
+
+from metahyper.exceptions import MissingDependencyError
+from metahyper.utils import instance_from_map
 
 
 @pytest.mark.parametrize(
-    "err", [
-        MissingDependencyError(libname="neps", dep="test_dep_1", install_group="group_1", cause=ImportError()),
-        MissingDependencyError(libname="neps", dep="test_dep_2", install_group=None, cause=ImportError())
-    ]
+    "err",
+    [
+        MissingDependencyError(
+            libname="neps",
+            dep="test_dep_1",
+            install_group="group_1",
+            cause=ImportError(),
+        ),
+        MissingDependencyError(
+            libname="neps", dep="test_dep_2", install_group=None, cause=ImportError()
+        ),
+    ],
 )
 @pytest.mark.metahyper
 def test_missing_dependancy_gets_flagged(err: MissingDependencyError) -> None:
     with pytest.raises(MissingDependencyError, match=err.dep):
         instance_from_map(mapping={}, request=err)
-
