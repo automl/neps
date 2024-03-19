6f764e7ee4fdd31f1e87cf7a7f555248b973018c
Daniel <63580393+danrgll@users.noreply.github.com>
2024-03-19 21:47:32 +0100

    fix python environment call in windows for test cases

--- tests/test_yaml_run_args/test_run_args_by_neps_run/test_neps_run.py
+++ tests/test_yaml_run_args/test_run_args_by_neps_run/test_neps_run.py
@@ -1,6 +1,7 @@
 import pytest
 import subprocess
 import os
+import sys
 BASE_PATH = "tests/test_yaml_run_args/test_run_args_by_neps_run/"
 
 
@@ -16,7 +17,7 @@ def test_run_with_yaml(yaml_file):
     assert os.path.exists(BASE_PATH + yaml_file), f"{yaml_file} does not exist."
 
     try:
-        subprocess.check_call(['python', BASE_PATH + 'neps_run.py', BASE_PATH +
+        subprocess.check_call([sys.executable, BASE_PATH + 'neps_run.py', BASE_PATH +
                                yaml_file])
     except subprocess.CalledProcessError:
         pytest.fail(f"NePS run failed for configuration: {yaml_file}")
