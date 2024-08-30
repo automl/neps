42468cddc58f3d55d22724171a128b97e4670226
eddiebergman <eddiebergmanhs@gmail.com>
2024-08-30 18:55:32 +0200

    fix: Timeout set explicitly to 120 seconds, debug on env vars

--- neps/env.py
+++ neps/env.py
@@ -3,16 +3,20 @@
 from __future__ import annotations
 
 import os
-from typing import Callable, TypeVar
+from typing import Any, Callable, TypeVar
 
 T = TypeVar("T")
 V = TypeVar("V")
+
+ENV_VARS_USED: dict[str, tuple[str, Any]] = {}
 
 
 def get_env(key: str, parse: Callable[[str], T], default: V) -> T | V:
     """Get an environment variable or return a default value."""
     if (e := os.environ.get(key)) is not None:
-        return parse(e)
+        value = parse(e)
+        ENV_VARS_USED[key] = (e, value)
+        return value
 
     return default
 
@@ -30,7 +34,7 @@ TRIAL_FILELOCK_POLL = get_env(
 TRIAL_FILELOCK_TIMEOUT = get_env(
     "NEPS_TRIAL_FILELOCK_TIMEOUT",
     parse=lambda e: None if is_nullable(e) else float(e),
-    default=None,
+    default=120,
 )
 
 JOBQUEUE_FILELOCK_POLL = get_env(
@@ -41,7 +45,7 @@ JOBQUEUE_FILELOCK_POLL = get_env(
 JOBQUEUE_FILELOCK_TIMEOUT = get_env(
     "NEPS_JOBQUEUE_FILELOCK_TIMEOUT",
     parse=lambda e: None if is_nullable(e) else float(e),
-    default=None,
+    default=120,
 )
 
 SEED_SNAPSHOT_FILELOCK_POLL = get_env(
@@ -52,7 +56,7 @@ SEED_SNAPSHOT_FILELOCK_POLL = get_env(
 SEED_SNAPSHOT_FILELOCK_TIMEOUT = get_env(
     "NEPS_SEED_SNAPSHOT_FILELOCK_TIMEOUT",
     parse=lambda e: None if is_nullable(e) else float(e),
-    default=None,
+    default=120,
 )
 
 OPTIMIZER_INFO_FILELOCK_POLL = get_env(
@@ -63,7 +67,7 @@ OPTIMIZER_INFO_FILELOCK_POLL = get_env(
 OPTIMIZER_INFO_FILELOCK_TIMEOUT = get_env(
     "NEPS_OPTIMIZER_INFO_FILELOCK_TIMEOUT",
     parse=lambda e: None if is_nullable(e) else float(e),
-    default=None,
+    default=120,
 )
 
 OPTIMIZER_STATE_FILELOCK_POLL = get_env(
@@ -74,7 +78,7 @@ OPTIMIZER_STATE_FILELOCK_POLL = get_env(
 OPTIMIZER_STATE_FILELOCK_TIMEOUT = get_env(
     "NEPS_OPTIMIZER_STATE_FILELOCK_TIMEOUT",
     parse=lambda e: None if is_nullable(e) else float(e),
-    default=None,
+    default=120,
 )
 
 GLOBAL_ERR_FILELOCK_POLL = get_env(
@@ -85,5 +89,5 @@ GLOBAL_ERR_FILELOCK_POLL = get_env(
 GLOBAL_ERR_FILELOCK_TIMEOUT = get_env(
     "NEPS_GLOBAL_ERR_FILELOCK_TIMEOUT",
     parse=lambda e: None if is_nullable(e) else float(e),
-    default=None,
+    default=120,
 )
