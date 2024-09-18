5a710303711e90c52deaadf6e3c78b889bbe4da1
eddiebergman <eddiebergmanhs@gmail.com>
2024-09-18 12:57:43 +0200

    fix: Remove old model

--- neps/optimizers/bayesian_optimization/models/__init__.py
+++ neps/optimizers/bayesian_optimization/models/__init__.py
@@ -1,9 +1,5 @@
-from neps.optimizers.bayesian_optimization.models.gp import ComprehensiveGP
-from neps.utils.common import MissingDependencyError
-
 from .ftpfn import FTPFNSurrogate
 
 SurrogateModelMapping = {
-    "gp": ComprehensiveGP,
     "ftpfn": FTPFNSurrogate,
 }
