2d36dbcc777d7fc921b14e46f5c62f5796638cc1
= <stolld@cs.uni-freiburg.de>
2024-10-25 15:41:22 +0100

    Removed latest with mike 2.1.3

--- latest/doc_yamls/run_pipeline_extended.py
+++
@@ -1,6 +0,0 @@
-
-def example_pipeline(learning_rate, optimizer, epochs, batch_size):
-    model = initialize_model()
-    training_loss = train_model(model, optimizer, learning_rate, epochs, batch_size)
-    evaluation_loss = evaluate_model(model)
-    return {"loss": evaluation_loss, "training_loss": training_loss}
