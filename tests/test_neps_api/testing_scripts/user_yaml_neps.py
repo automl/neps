5a93ec34e26d43f2c6433e6ad22d86b3b70dd163
Meganton <gebureka@cs.uni-freiburg.de>
2024-12-31 02:04:12 +0100

    renamed run_pipeline in user_yaml_neps.py

--- tests/test_neps_api/testing_scripts/user_yaml_neps.py
+++ tests/test_neps_api/testing_scripts/user_yaml_neps.py
@@ -12,6 +12,10 @@ pipeline_space = dict(
 
 def run_pipeline(val1, val2):
     warn("run_pipeline is deprecated, use evaluate_pipeline instead", DeprecationWarning)
+    return evaluate_pipeline(val1, val2)
+
+
+def evaluate_pipeline(val1, val2):
     objective_to_minimize = val1 * val2
     return objective_to_minimize
 
