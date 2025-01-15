da1b7d8e8146e1e9844c5c6732bc5ba33ea53279
eddiebergman <eddiebergmanhs@gmail.com>
2025-01-15 19:27:42 +0100

    feat: improve imports

--- neps/__init__.py
+++ neps/__init__.py
@@ -1,16 +1,25 @@
 from neps.api import run
+from neps.optimizers import algorithms
+from neps.optimizers.ask_and_tell import AskAndTell
+from neps.optimizers.optimizer import SampledConfig
 from neps.plot.plot import plot
 from neps.plot.tensorboard_eval import tblogger
 from neps.space import Categorical, Constant, Float, Integer, SearchSpace
+from neps.state import BudgetInfo, Trial
 from neps.status.status import get_summary_dict, status
 from neps.utils.files import load_and_merge_yamls as load_yamls
 
 __all__ = [
+    "AskAndTell",
+    "BudgetInfo",
     "Categorical",
     "Constant",
     "Float",
     "Integer",
+    "SampledConfig",
     "SearchSpace",
+    "Trial",
+    "algorithms",
     "get_summary_dict",
     "load_yamls",
     "plot",
