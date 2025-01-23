6d9ea56a0380d0576999d798ecd2e0383a42c629
vladislavalerievich <vlad.moroshan@gmail.com>
2025-01-23 14:40:42 +0100

    Update imports

--- tests/test_optimization_over_graphs.py
+++ tests/test_optimization_over_graphs.py
@@ -15,7 +15,7 @@ from gpytorch.kernels import AdditiveKernel, MaternKernel, ScaleKernel
 from grakel_replace.optimize import optimize_acqf_graph
 from grakel_replace.optimize import sample_graphs
 from grakel_replace.torch_wl_kernel import BoTorchWLKernel
-from grakel_replace.torch_wl_kernel import set_graph_lookup
+from grakel_replace.context_managers import set_graph_lookup
 from grakel_replace.utils import min_max_scale
 
 
