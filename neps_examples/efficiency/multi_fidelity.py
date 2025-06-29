459cb884c52522f22ffcd16427733dab60dde84b
Ema Mekic <emica.mekic@gmail.com>
2025-06-29 19:37:45 +0200

    Mf example

--- neps_examples/efficiency/multi_fidelity.py
+++ neps_examples/efficiency/multi_fidelity.py
@@ -94,5 +94,5 @@ neps.run(
     root_directory="results/multi_fidelity_example",
     # Optional: Do not start another evaluation after <=50 epochs, corresponds to cost
     # field above.
-    cost_to_spend=10,
+    fidelities_to_spend=10
 )
