44ce3203b73bce4cae1eba95a9df9e2ffb4157fb
Nastaran Alipour <nastaran.alipour78@gmail.com>
2025-07-29 15:03:58 +0200

    add async evaluation

---
+++ neps_examples/convenience/async_evaluation/submit.py
@@ -0,0 +1,49 @@
+from pathlib import Path
+import subprocess
+import neps
+import os
+
+def _submit_job(pipeline_directory: Path, script: str):
+    script_path = pipeline_directory / "submit.sh"
+    print(f"Submitting the script {script_path} (see below): \n\n{script}")
+
+    # You may want to remove the below check and not ask before submitting every time
+    if input("Ok to submit? [Y|n] -- ").lower() in {"y", ""}:
+        script_path.write_text(script)
+        os.system(f"sbatch {script_path}")
+    else:
+        raise ValueError("We generated a slurm script that should not be submitted.")
+
+
+def evaluate_pipeline_via_slurm(pipeline_id, pipeline_directory, lr, optimizer):
+    print(f"{pipeline_id} optimization_dir")
+    out_dir = Path('output_dir')
+    out_dir.mkdir(parents=True, exist_ok=True)
+
+    script = f"""#!/bin/bash
+#SBATCH --job-name=mnist_toy
+#SBATCH --partition=bosch_cpu-cascadelake
+#SBATCH --output={out_dir}/%j.out
+#SBATCH --error={out_dir}/%j.err
+
+python run_pipeline.py --learning_rate {lr} \\
+                       --optimizer {optimizer} \\
+                       --root_directory {"results/slurm_script_example"} \\
+                       --pipeline_id {pipeline_id}
+"""
+
+    return _submit_job(pipeline_directory, script)
+
+
+pipeline_space = dict(
+    optimizer=neps.Categorical(choices=["sgd", "adam"]),
+    lr=neps.Float(lower=10e-7, upper=10e-3, log=True),
+)
+
+neps.run(
+    evaluate_pipeline=evaluate_pipeline_via_slurm,
+    pipeline_space=pipeline_space,
+    root_directory="results/slurm_script_example",
+    max_evaluations_total=1,
+    post_run_summary=False,
+)
